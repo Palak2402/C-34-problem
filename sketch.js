a5a858452120d199efe5abd64df7808248891cfc
Palak2402 <63538659+Palak2402@users.noreply.github.com>
2020-08-09 15:03:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+//Create variables here
+var dog,happydog;
+var database;
+var foodS,foodStock;
+
+function preload()
+{
+  //load images here
+  
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
